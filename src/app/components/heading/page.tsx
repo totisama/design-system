8cadc66e9a72a77456e292734f63df7f072cb5e4
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-24 19:00:57 +0200

    modify heading page

--- src/app/components/heading/page.tsx
+++ src/app/components/heading/page.tsx
@@ -1,6 +1,14 @@
+import { Heading } from '@/components/heading'
 import { PageWrapper } from '@/components/page-wrapper'
+import { Playground } from '@/components/playground'
 import type React from 'react'
 
 export default function HeadingPage() {
-  return <PageWrapper name='Heading'>Heading</PageWrapper>
+  return (
+    <PageWrapper name='Heading'>
+      <Playground>
+        <Heading as='h1'>This is a heading</Heading>
+      </Playground>
+    </PageWrapper>
+  )
 }
