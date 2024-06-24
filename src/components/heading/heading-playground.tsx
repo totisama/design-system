fbb6173d751e2e5756522ef960d4eec8c30f193e
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-24 21:01:27 +0200

    add const

--- src/components/heading/heading-playground.tsx
+++ src/components/heading/heading-playground.tsx
@@ -4,6 +4,8 @@ import { Heading, type HeadingTags } from '@/components/heading/heading'
 import { Container } from '@/components/container'
 import { type ChangeEvent, useState } from 'react'
 
+const HEADING_OPTIONS = ['h1', 'h2', 'h3', 'h4', 'h5']
+
 export const HeadingPlayground = () => {
   const [as, setAs] = useState<HeadingTags>('h1')
   const [text, setText] = useState('This is a heading')
@@ -32,11 +34,14 @@ export const HeadingPlayground = () => {
             onChange={handleAsChange}
             className='p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent'
           >
-            <option value='h1'>h1</option>
-            <option value='h2'>h2</option>
-            <option value='h3'>h3</option>
-            <option value='h4'>h4</option>
-            <option value='h5'>h5</option>
+            {HEADING_OPTIONS.map((option) => (
+              <option
+                key={option}
+                value={option}
+              >
+                {option}
+              </option>
+            ))}
           </select>
         </div>
         <div className='flex flex-col my-3 w-1/2'>
