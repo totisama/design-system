741f3f4e90038ba1c5d336d626007174b1f74839
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-24 20:58:25 +0200

    add padding

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -3,7 +3,7 @@ import type React from 'react'
 export const Container = ({ children }: { children: React.ReactNode }) => {
   return (
     <section className='h-96 w-full'>
-      <div className='w-full bg-[#F0F3F4] h-4/5 flex text-center items-center justify-center rounded-2xl'>
+      <div className='w-full bg-[#F0F3F4] px-5 h-4/5 flex text-center items-center justify-center rounded-2xl'>
         {children}
       </div>
     </section>
