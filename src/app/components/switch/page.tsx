1d79f2074c36d67dc9560ece8c69530854b61c52
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-24 18:49:22 +0200

    add paragraph

--- src/app/components/switch/page.tsx
+++ src/app/components/switch/page.tsx
@@ -1,10 +1,19 @@
 import { PageWrapper } from '@/components/page-wrapper'
 import { Switch } from '@/components/switch'
+import { Text } from '@/components/text'
 import type React from 'react'
 
 export default function SwitchPage() {
   return (
     <PageWrapper name='Switch'>
+      <section className='w-full my-10'>
+        <Text as='p'>
+          A toggle is a control used to quickly switch between two possible
+          states. Toggles are designed for binary actions that take effect
+          immediately after the user &quot;flips the switch&quot;. They are
+          commonly used for &quot;on/off&quot; switches.
+        </Text>
+      </section>
       <section className='h-96 w-full'>
         <div className='w-full bg-[#F0F3F4] h-4/5 flex items-center justify-center rounded-2xl'>
           <Switch />
