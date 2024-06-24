e261ddc0b3a2bddf73f626e00321f32bc0fd4db4
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-24 22:32:56 +0200

    fix htmlFor

--- src/components/help-text/help-text-playground.tsx
+++ src/components/help-text/help-text-playground.tsx
@@ -43,7 +43,7 @@ export const HelpTextPlayground = () => {
           </select>
         </div>
         <div className='flex flex-col my-3 w-1/2'>
-          <label htmlFor='type'>Text:</label>
+          <label htmlFor='text'>Text:</label>
           <input
             id='text'
             name='text'
