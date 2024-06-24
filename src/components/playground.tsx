01f9496e93b65d4ba09c0820b05f14789a3c72a0
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-24 20:09:46 +0200

    add text center

--- src/components/playground.tsx
+++ src/components/playground.tsx
@@ -3,7 +3,7 @@ import type React from 'react'
 export const Playground = ({ children }: { children: React.ReactNode }) => {
   return (
     <section className='h-96 w-full'>
-      <div className='w-full bg-[#F0F3F4] h-4/5 flex items-center justify-center rounded-2xl'>
+      <div className='w-full bg-[#F0F3F4] h-4/5 flex text-center items-center justify-center rounded-2xl'>
         {children}
       </div>
     </section>
