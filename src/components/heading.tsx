ba8e114d0cea25eabafe2870195c6655584bc744
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-24 20:09:40 +0200

    export type

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,7 +1,7 @@
 import type React from 'react'
 import { Text } from '@/components/text'
 
-type HeadingTags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5'
+export type HeadingTags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5'
 
 interface HeadinProps {
   as: HeadingTags
