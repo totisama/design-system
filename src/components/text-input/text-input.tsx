9bfaa82e2159071bc61796a4896867efe33a934d
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-25 10:52:36 +0200

    change border and background color

--- src/components/text-input/text-input.tsx
+++ src/components/text-input/text-input.tsx
@@ -48,7 +48,7 @@ export const TextInput = ({
         onChange={onClick}
         placeholder={placeholder}
         disabled={disabled}
-        className={`w-full p-2 rounded-lg border border-gray-300 ${variantStyle} focus:outline-none focus:ring-2 focus:ring-border-interactive focus:border-transparent placeholder:text-text-placeholder`}
+        className={`w-full p-2 rounded-lg border border-border-subtle-00 ${variantStyle} focus:outline-none focus:ring-2 focus:ring-border-interactive focus:border-transparent disabled:bg-background-hover placeholder:text-text-placeholder`}
       />
       {helpText && (
         <HelpText
