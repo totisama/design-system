5babdf4716033d36d996a67b9442abcfb8fbf0ef
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-26 12:10:04 +0200

    create file for text types + import them

---
+++ src/components/text/text-types.ts
@@ -0,0 +1,18 @@
+export type TextTags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'p'
+
+export type TextSizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+
+export type FontWeight = 'normal' | 'bold'
+
+export type LetterSpacing = 'normal' | 'wide'
+
+export type LineHeight = 'tight' | 'normal' | 'relaxed' | 'loose'
+
+export type TextColor =
+  | 'text-primary'
+  | 'text-secondary'
+  | 'text-placeholder'
+  | 'text-link'
+  | 'text-error'
+  | 'text-button'
+  | 'text-success'
--- src/components/text/text.tsx
+++ src/components/text/text.tsx
@@ -7,20 +7,14 @@ import {
   textSpacing,
   textWeight,
 } from '@/components/text/text-styles'
-
-export type TextTags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'p'
-export type TextSizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
-export type FontWeight = 'normal' | 'bold'
-export type LetterSpacing = 'normal' | 'wide'
-export type LineHeight = 'tight' | 'normal' | 'relaxed' | 'loose'
-export type TextColor =
-  | 'text-primary'
-  | 'text-secondary'
-  | 'text-placeholder'
-  | 'text-link'
-  | 'text-error'
-  | 'text-button'
-  | 'text-success'
+import {
+  type FontWeight,
+  type LetterSpacing,
+  type LineHeight,
+  type TextColor,
+  type TextSizes,
+  type TextTags,
+} from '@/components/text/text-types'
 
 interface TextProps {
   as: TextTags
