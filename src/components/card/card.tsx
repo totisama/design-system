083ad0481922af627e263a7e142b0c8df58c1e49
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-28 00:08:03 +0200

    remove unused prop

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,23 +1,17 @@
 import type React from 'react'
 import { Box } from '@/components/box/box'
-import {
-  type BackgroundColors,
-  type Width,
-  type Height,
-} from '@/components/box/box-tokens'
+import { type BackgroundColors, type Width } from '@/components/box/box-tokens'
 
 export interface CardProps {
   children: React.ReactNode
   backgroundColor?: BackgroundColors
   width?: Width
-  height?: Height
 }
 
 export const Card = ({
   children,
   backgroundColor = 'background',
   width,
-  height,
 }: CardProps) => {
   return (
     <Box
