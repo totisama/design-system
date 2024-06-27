728469f5cf5234d9f290661e6b3dd48021fb3981
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-27 19:53:32 +0200

    fix title

--- src/app/components/radio-button/page.tsx
+++ src/app/components/radio-button/page.tsx
@@ -3,9 +3,9 @@ import { Text } from '@/components/text/text'
 import type React from 'react'
 import { RadioButtonPlayground } from '@/components/radio-button/radio-button-playground'
 
-export default function SwitchPage() {
+export default function RadioButtonPage() {
   return (
-    <PageWrapper name='Switch'>
+    <PageWrapper name='Radio Button'>
       <section className='w-full my-5'>
         <Text as='p'>
           Radio buttons are used for exclusive choices, not for multiple
