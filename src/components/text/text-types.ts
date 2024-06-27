b12cf354da9275cb7dc83c747a80e5a5e7104162
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-27 20:09:10 +0200

    Change default text styles

--- src/components/text/text-types.ts
+++ src/components/text/text-types.ts
@@ -1,4 +1,12 @@
-export type TextTags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'p'
+export type TextTags =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'p'
+  | 'subtitle'
+  | 'body'
 
 export type TextSizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
 
