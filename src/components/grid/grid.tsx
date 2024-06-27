9a33a7b4c2f721a12bba84c9c852b2e7e66c0303
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-27 23:29:07 +0200

    modify grid

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -9,58 +9,27 @@ export const Grid = () => {
     <Box
       as='div'
       padding='spacing-04'
+      display='flex'
+      flexDirection='column'
+      gapY='spacing-10'
     >
+      <Flex
+        width={'100%'}
+        justifyContent='center'
+        as='div'
+      >
+        <Image
+          src={'/images/grid/grid.png'}
+          alt='image'
+          width={500}
+          height={500}
+        />
+      </Flex>
       <Flex
         direction='row'
         width={'100%'}
         as='div'
       >
-        <Flex
-          direction='column'
-          gapY='spacing-04'
-          width={'33%'}
-          as='div'
-        >
-          <Stack
-            orientation='vertical'
-            as='div'
-            spacing='spacing-03'
-          >
-            <Text as='h5'>Grid</Text>
-            <Image
-              src={'/images/grid/grid.png'}
-              alt='image'
-              width={250}
-              height={250}
-            />
-          </Stack>
-          <Stack
-            orientation='vertical'
-            as='div'
-            spacing='spacing-03'
-          >
-            <Text as='h5'>Two columns short / large</Text>
-            <Image
-              src={'/images/grid/large.png'}
-              alt='image'
-              width={250}
-              height={250}
-            />
-          </Stack>
-          <Stack
-            orientation='vertical'
-            as='div'
-            spacing='spacing-03'
-          >
-            <Text as='h5'>Four columns</Text>
-            <Image
-              src={'/images/grid/Four columns.png'}
-              alt='image'
-              width={250}
-              height={250}
-            />
-          </Stack>
-        </Flex>
         <Flex
           direction='column'
           gapY='spacing-04'
@@ -121,6 +90,39 @@ export const Grid = () => {
             <Text as='h5'>Three columns</Text>
             <Image
               src={'/images/grid/Three columns.png'}
+              alt='image'
+              width={250}
+              height={250}
+            />
+          </Stack>
+        </Flex>
+        <Flex
+          direction='column'
+          gapY='spacing-04'
+          width={'33%'}
+          as='div'
+        >
+          <Stack
+            orientation='vertical'
+            as='div'
+            spacing='spacing-03'
+          >
+            <Text as='h5'>Two columns short / large</Text>
+            <Image
+              src={'/images/grid/large.png'}
+              alt='image'
+              width={250}
+              height={250}
+            />
+          </Stack>
+          <Stack
+            orientation='vertical'
+            as='div'
+            spacing='spacing-03'
+          >
+            <Text as='h5'>Four columns</Text>
+            <Image
+              src={'/images/grid/Four columns.png'}
               alt='image'
               width={250}
               height={250}
