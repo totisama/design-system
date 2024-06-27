438619df4b3015b11001181c0b7cae45a0c6ba40
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-28 01:47:23 +0200

    Add input images

--- src/app/components/text-input/page.tsx
+++ src/app/components/text-input/page.tsx
@@ -1,8 +1,10 @@
+import { Flex } from '@/components/flex/flex'
 import { PageWrapper } from '@/components/page-wrapper'
 import { Paragraph } from '@/components/paragraph/paragraph'
 import { Stack } from '@/components/stack/stack'
 import { TextInputPlayground } from '@/components/text-input/text-input-playground'
 import { Text } from '@/components/text/text'
+import Image from 'next/image'
 import type React from 'react'
 
 const TEXT =
@@ -18,6 +20,81 @@ export default function TextInputPage() {
       >
         <Paragraph text={TEXT} />
         <TextInputPlayground />
+        <Flex
+          direction='row'
+          width={'100%'}
+          justifyContent='center'
+          gapX='spacing-06'
+          as='div'
+        >
+          <Flex
+            direction='column'
+            gapY='spacing-04'
+            width={'33%'}
+            as='div'
+          >
+            <Stack
+              orientation='vertical'
+              as='div'
+              spacing='spacing-03'
+            >
+              <Text as='h5'>Import</Text>
+              <Image
+                src={'/images/input/False.png'}
+                alt='image'
+                width={500}
+                height={500}
+              />
+            </Stack>
+            <Stack
+              orientation='vertical'
+              as='div'
+              spacing='spacing-03'
+            >
+              <Text as='h5'>Password</Text>
+              <Image
+                src={'/images/input/False2.png'}
+                alt='image'
+                width={500}
+                height={500}
+              />
+            </Stack>
+          </Flex>
+          <Flex
+            direction='column'
+            gapY='spacing-04'
+            width={'33%'}
+            as='div'
+          >
+            <Stack
+              orientation='vertical'
+              as='div'
+              spacing='spacing-03'
+            >
+              <Text as='h5'>Search</Text>
+              <Image
+                src={'/images/input/False4.png'}
+                alt='image'
+                width={500}
+                height={500}
+              />
+            </Stack>
+
+            <Stack
+              orientation='vertical'
+              as='div'
+              spacing='spacing-03'
+            >
+              <Text as='h5'>Password visible</Text>
+              <Image
+                src={'/images/input/True3.png'}
+                alt='image'
+                width={500}
+                height={500}
+              />
+            </Stack>
+          </Flex>
+        </Flex>
         <Stack
           as='div'
           orientation='vertical'
