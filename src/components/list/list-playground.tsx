a3691958fa395175c24e1e0d6c65b9bc382a8465
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-27 18:36:08 +0200

    Add list component + playground

---
+++ src/components/list/list-playground.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { Container } from '@/components/container'
+import { List, type ListType } from './list'
+import { type ChangeEvent, useState } from 'react'
+import { Box } from '../box/box'
+
+const ITEMS = [
+  { text: 'Item 1' },
+  { text: 'Item 2', children: ['Child 1', 'Child 2', 'Child 3'] },
+  { text: 'Item 3' },
+]
+
+const TYPE = ['ordered', 'unordered']
+
+export const ListPlayground = () => {
+  const [type, setType] = useState<ListType>('unordered')
+
+  const handleAsChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setType(e.target.value as ListType)
+  }
+
+  return (
+    <>
+      <Box
+        as='section'
+        width={'100%'}
+        display='flex'
+        gap='spacing-06'
+      >
+        <Box
+          as='div'
+          marginY='spacing-04'
+          width={'50%'}
+          display='flex'
+          flexDirection='column'
+          gap='spacing-03'
+        >
+          <label htmlFor='type'>Type:</label>
+          <select
+            id='type'
+            name='type'
+            onChange={handleAsChange}
+            defaultValue={type}
+            className='p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent'
+          >
+            {TYPE.map((option) => (
+              <option
+                key={option}
+                value={option}
+              >
+                {option}
+              </option>
+            ))}
+          </select>
+        </Box>
+      </Box>
+      <Container
+        justifyContent='center'
+        alignItems='center'
+      >
+        <List
+          items={ITEMS}
+          type={type}
+        />
+      </Container>
+    </>
+  )
+}
