0f2ed6fadc8721a83d8ecaaf44ab98e7b7e24cec
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-27 23:39:49 +0200

    remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import { Box } from '@/components/box/box'
 import { HomeWrapper } from '@/components/home-wrapper'
 import { Stack } from '@/components/stack/stack'
-import { Text } from '@/components/text/text'
 import Image from 'next/image'
 
 export default function HomePage() {
