c16c040a95e8e53d63be456a29812a8dbae8b9e9
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-27 22:48:54 +0200

    Add grid page

--- src/app/elements/grid/page.tsx
+++ src/app/elements/grid/page.tsx
@@ -1,6 +1,52 @@
+import { Grid } from '@/components/grid/grid'
 import { PageWrapper } from '@/components/page-wrapper'
+import { Paragraph } from '@/components/paragraph/paragraph'
+import { Stack } from '@/components/stack/stack'
+import { Text } from '@/components/text/text'
 import type React from 'react'
 
+const TEXT =
+  'Spacing is the negative area between elements and components. It is commonly controlled in code with margin and padding properties, applied to both vertical and horizontal edges.'
+
 export default function GridPage() {
-  return <PageWrapper name='Grid'>Grid</PageWrapper>
+  return (
+    <PageWrapper name='Grid'>
+      <Stack
+        as='div'
+        orientation='vertical'
+        spacing='spacing-07'
+      >
+        <Paragraph text={TEXT} />
+        <Grid />
+      </Stack>
+      <Stack
+        as='div'
+        orientation='vertical'
+        spacing='spacing-06'
+      >
+        <Stack
+          as='div'
+          orientation='vertical'
+          spacing='spacing-01'
+        >
+          <Text as='h5'>Good Practices</Text>
+          <ul className='list-disc ml-6'>
+            <li>
+              We suggest using a grid of 14 columns, letting the first column
+              and the last one as horizontal margins.{' '}
+            </li>
+            <li>
+              For one column content, use the first two columns and the last two
+              columns as margins.
+            </li>
+            <li>Use $spacing-12 (96px) for vertical margins.</li>
+            <li>
+              Dividing the available space in 12 columns is useful because it
+              can be divided by 2, 3, 4, or even 6 small columns.
+            </li>
+          </ul>
+        </Stack>
+      </Stack>
+    </PageWrapper>
+  )
 }
