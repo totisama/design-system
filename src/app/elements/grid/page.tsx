8506b0b7c1792ec2b486623b9acb5348b7af45f9
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-27 23:01:48 +0200

    small fix

--- src/app/elements/grid/page.tsx
+++ src/app/elements/grid/page.tsx
@@ -18,33 +18,33 @@ export default function GridPage() {
       >
         <Paragraph text={TEXT} />
         <Grid />
-      </Stack>
-      <Stack
-        as='div'
-        orientation='vertical'
-        spacing='spacing-06'
-      >
         <Stack
           as='div'
           orientation='vertical'
-          spacing='spacing-01'
+          spacing='spacing-06'
         >
-          <Text as='h5'>Good Practices</Text>
-          <ul className='list-disc ml-6'>
-            <li>
-              We suggest using a grid of 14 columns, letting the first column
-              and the last one as horizontal margins.{' '}
-            </li>
-            <li>
-              For one column content, use the first two columns and the last two
-              columns as margins.
-            </li>
-            <li>Use $spacing-12 (96px) for vertical margins.</li>
-            <li>
-              Dividing the available space in 12 columns is useful because it
-              can be divided by 2, 3, 4, or even 6 small columns.
-            </li>
-          </ul>
+          <Stack
+            as='div'
+            orientation='vertical'
+            spacing='spacing-01'
+          >
+            <Text as='h5'>Good Practices</Text>
+            <ul className='list-disc ml-6'>
+              <li>
+                We suggest using a grid of 14 columns, letting the first column
+                and the last one as horizontal margins.{' '}
+              </li>
+              <li>
+                For one column content, use the first two columns and the last
+                two columns as margins.
+              </li>
+              <li>Use $spacing-12 (96px) for vertical margins.</li>
+              <li>
+                Dividing the available space in 12 columns is useful because it
+                can be divided by 2, 3, 4, or even 6 small columns.
+              </li>
+            </ul>
+          </Stack>
         </Stack>
       </Stack>
     </PageWrapper>
